65d8c5c858acecf24e817d8785782cd701783ee7
prathap443 <prathap443@gmail.com>
2025-05-17 08:54:41 +0100

    Add Dashboard route to fix blank screen

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import TradePage from './pages/TradePage';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/trade/:symbol" element={<TradePage />} />
       </Routes>
     </HashRouter>
